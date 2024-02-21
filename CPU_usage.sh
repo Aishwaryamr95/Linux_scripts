a51a463742ba6a183baa91b1c2abe5ec7ba884c3
Aishwarya <109503119+Aishwaryamr95@users.noreply.github.com>
2024-02-21 17:36:00 +0530

    Create CPU_usage.sh

---
+++ CPU_usage.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CPU=$(sar 1 5 | grep "Average" | sed 's/^.* //')
+CPU=$( printf "%.0f" $CPU )
+
+if [ "$CPU" -lt 20 ]
+then
+        echo "CPU usage is high!" | sendmail admin@example.com
+fi
